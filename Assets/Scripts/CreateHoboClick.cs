b0bd408b23a6da834b37913ed4d7c146b31f2475
Vladimir Moroz <vovanmoroz1509@gmail.com>
2020-01-31 13:02:31 +0200

    Added BumClass (Class) and Traits (Class)

---
+++ Assets/Scripts/CreateHoboClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateHoboClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject hobo = null;
+   
+
+    public void onClickHobo()
+    {
+        Debug.Log("Clicked");
+        BumClass bum = new BumClass();
+        bum.BumInit(1,hobo);
+        Instantiate(hobo, new Vector3(0, 0,15), transform.rotation);
+
+    }
+}
---
+++ Assets/Scripts/Traits.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traits : MonoBehaviour
+{
+    //CLASS VARIABLES
+    public int moddedStat;
+    public int statMod;
+    public bool good;
+  
+
+
+    //CONSTRUCTOR/DESTRUCTOR
+    public Traits(bool tr)
+    {
+        if(tr == true)
+        {
+            moddedStat = Random.Range(1, 5);
+        }
+        else if(tr == false)
+        {
+            moddedStat = -Random.Range(1, 5);
+        }
+        
+        statMod = Random.Range(0,3);
+        good = tr;
+    }
+    
+    void WildCard(){}
+
+}
+
+
+
