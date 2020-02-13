262112c3fdd5774682395c0dcc1b2f65f12f0ae9
Vladimir Moroz <vovanmoroz1509@gmail.com>
2020-02-13 14:35:57 +0200

    Minor tweaks

--- Assets/Scripts/RatClass.cs
+++ Assets/Scripts/RatClass.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class RatClass : MonoBehaviour
 {
     // Start is called before the first frame update¨
-    int HP  = 25;
+    public int HP  = 25;
+    public int damage = 5;
     private void OnCollisionEnter(Collision hit) 
     {
         if(hit.gameObject.name == "TestShot(Clone)")
         {
             Debug.Log("Rat took damage");
+            HP -= damage;
+            if(HP <= 0)
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 }
