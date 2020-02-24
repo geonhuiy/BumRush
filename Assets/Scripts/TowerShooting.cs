67e67f21176d9b59637705fe8519f01ecc6db933
Jamie <rkawk3316@gmail.com>
2020-02-24 17:38:17 +0200

    Fixed turret auto targeting

---
+++ Assets/Scripts/TowerShooting.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShooting : MonoBehaviour
+{
+    private GameObject targetRat;
+    private GameObject[] targets;
+    [SerializeField]
+    private GameObject projectile;
+    private float towerRange = 16f;
+    private float towerRatDistance, attackCooldown;
+    void Update()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Rat");
+        targetRat = FindClosestEnemy(targets);
+
+        // targetRat = GameObject.FindGameObjectWithTag("Rat");
+        if (targetRat != null)
+        {
+            towerRatDistance = Vector3.Distance(targetRat.transform.position, this.transform.position);
+            if (IsInRange())
+            {
+                Debug.DrawLine(transform.position, targetRat.transform.position, Color.red);
+
+                AttackRat();
+            }
+        }
+    }
+
+    private bool IsInRange()
+    {
+        if (towerRatDistance <= towerRange)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void AttackRat()
+    {
+        transform.LookAt(targetRat.transform);
+        attackCooldown -= Time.deltaTime;
+        if (attackCooldown <= 0)
+        {
+            Debug.Log("Hobo ready");
+            Instantiate(projectile, transform.position + 2 * transform.forward, transform.rotation);
+            attackCooldown = 2;
+        }
+    }
+
+    /*private void FindNewRat()
+    {
+        targetRat = null;
+        targetRat = GameObject.FindGameObjectWithTag("Rat");
+    }*/
+
+    private GameObject FindClosestEnemy(GameObject[] enemies)
+    {
+        GameObject bestTarget = null;
+        float closestDistSqr = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (GameObject potentialTarget in enemies)
+        {
+            Vector3 directionToTarget = potentialTarget.gameObject.transform.position - currentPos;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistSqr)
+            {
+                closestDistSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+        return bestTarget;
+    }
+}
