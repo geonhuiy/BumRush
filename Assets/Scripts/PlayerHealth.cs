8b4a7c27f1fa3c3a16449ee46810352a43cfa173
michael.bell <michael.bell@metropolia.fi>
2020-02-25 23:04:46 +0200

    Fannied about

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,20 +6,20 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
 
-    private int health = 5;
+    private int health = 1;
     public Text healthText;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "HEALTH  : " + health;
+        healthText.text = "Tent Health  : " + health;
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
