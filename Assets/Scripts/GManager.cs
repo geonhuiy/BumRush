cd02c1ff5f982bcb7565fe683eab8349131150f2
Jamie <rkawk3316@gmail.com>
2020-02-28 13:36:25 +0200

    Added GameManager and in-game money count

---
+++ Assets/Scripts/GManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GManager : MonoBehaviour
+{
+    public static GManager gManagerInstance;
+    [SerializeField]
+    private Text moneyText;
+    public int money;
+    private void Awake()
+    {
+        if (gManagerInstance == null) {
+            gManagerInstance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+    private void Start()
+    {
+        money = 10;
+    }
+    private void Update()
+    {
+        moneyText.text = money.ToString();
+    }
+}
