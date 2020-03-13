102be4f91d196eb3e45ff9db49ea8f255d15ed4a
Jamie <rkawk3316@gmail.com>
2020-03-13 11:38:56 +0200

    Actually increased price increment for level2 and level3 towers

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -209,11 +209,11 @@ public class TowerManager : MonoBehaviour
                 hobo1.text = lvl1price + "$" + "\nHOBO";
             break;
             case 2:
-            lvl2price += 2;
+            lvl2price += 4;
                 hobo2.text = lvl2price + "$" + "\nHOBO";
             break;
             case 3:
-            lvl3price += 2;
+            lvl3price += 10;
                 hobo3.text = lvl3price + "$" + "\nHOBO";
             break;
         }
